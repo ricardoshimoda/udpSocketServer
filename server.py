e707d3fd4f550db684b9c746becde17bf325c3b3
rshimoda <rshimoda@gmail.com>
2020-01-14 16:52:14 -0500

    changes to implement uniform message object

--- server.py
+++ server.py
@@ -11,23 +11,33 @@ connected = 0
 
 clients = {}
 
+# this is the receiving message loop 
 def connectionLoop(sock):
    while True:
       data, addr = sock.recvfrom(1024)
       data = str(data)
       if addr in clients:
+         # received a heartbeat
          if 'heartbeat' in data:
+            #updates heartbeat data to make sure client is still alive
             clients[addr]['lastBeat'] = datetime.now()
       else:
+         # new client - receives a connect
          if 'connect' in data:
+            # This deal with new connections
             clients[addr] = {}
             clients[addr]['lastBeat'] = datetime.now()
-            clients[addr]['color'] = 0
-            message = {"cmd": 0,"player":{"id":str(addr)}}
+            clients[addr]['color'] = {"R": random.random(), "G": random.random(), "B": random.random()}
+            # Let's start colorful
+            message = {"cmd": 0,"players":[{"id":str(addr), "color": clients[addr]['color']}]}
             m = json.dumps(message)
+            # Sends information of the new connected client to everyone
             for c in clients:
                sock.sendto(bytes(m,'utf8'), (c[0],c[1]))
+            
+            sock.sentto(bytes(m,'utf8'), ())
 
+# Every second verifies if clients are still active or not based on their heartbeat
 def cleanClients():
    while True:
       for c in list(clients.keys()):
@@ -38,13 +48,16 @@ def cleanClients():
             clients_lock.release()
       time.sleep(1)
 
+# Every second sends message about whoelse is still connected to the server
 def gameLoop(sock):
    while True:
+      # This sends the UPDATE (1) message to the client
       GameState = {"cmd": 1, "players": []}
       clients_lock.acquire()
       print (clients)
       for c in clients:
          player = {}
+         #Change color
          clients[c]['color'] = {"R": random.random(), "G": random.random(), "B": random.random()}
          player['id'] = str(c)
          player['color'] = clients[c]['color']
