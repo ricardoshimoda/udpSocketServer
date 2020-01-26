eeb418a62d555b9d572784e2cac78ffbfc259468
root <root@ip-172-31-88-170.ec2.internal>
2020-01-26 23:53:43 +0000

    Position

--- server.py
+++ server.py
@@ -21,8 +21,12 @@ def connectionLoop(sock):
          if 'heartbeat' in data:
             #updates heartbeat data to make sure client is still alive
             clients[addr]['lastBeat'] = datetime.now()
-         if 'position' in data
-            clients[addr]['position'] = 
+         if 'cube_position' in data:
+            print(data)
+            positionMessage = data[2:-1]
+            print(positionMessage)
+            positionData = json.JSONDecoder().decode(positionMessage)
+            clients[addr]['position'] = positionData['position']
       else:
          # new client - receives a connect
          if 'connect' in data:
@@ -32,8 +36,8 @@ def connectionLoop(sock):
             clients[addr]['color'] = {"R": random.random(), "G": random.random(), "B": random.random()}
             # Calculate the position of the newly acquired member
             finalCount = connected + 1
-            xCoord = ((int)finalCount/2) * xStep
-            if finalCount%2 == 1:
+            xCoord = (finalCount//2) * xStep
+            if (int(finalCount) % 2 == 1):
                xCoord = -1 * xCoord
             clients[addr]['position'] = {"x": xCoord,"y":0.0, "z":0.0}
             # Sends information of the new connected client to everyone - but the newly connected client
@@ -41,12 +45,12 @@ def connectionLoop(sock):
             m = json.dumps(message)
             for c in clients:
                if c != addr :
-                  print('NEW messsage: ')
-                  print(m)
-                  print('**************************************')
+                  #print('NEW messsage: ')
+                  #print(m)
+                  #print('**************************************')
                   sock.sendto(bytes(m,'utf8'), (c[0],c[1]))
             
-            print("Now going to the other message")
+            #print("Now going to the other message")
             # Sends information of all connected clients to the newly connected client
             Others = {"cmd": 2, "players": []}
             for c in clients:
@@ -56,17 +60,19 @@ def connectionLoop(sock):
                player['position'] = clients[c]['position']
                Others['players'].append(player)
             oth=json.dumps(Others)
-            print('OTHERS messsage: ')
-            print(oth)
-            print('**************************************')
+            #print('OTHERS messsage: ')
+            #print(oth)
+            #print('**************************************')
             sock.sendto(bytes(oth,'utf8'), (addr[0], addr[1]))
 
 # Every second verifies if clients are still active or not based on their heartbeat
 def cleanClients(sock):
    while True:
+      needToSend = False
       deleteMessage = {"cmd": 3,"players":[]}
       for c in list(clients.keys()):
          if (datetime.now() - clients[c]['lastBeat']).total_seconds() > 5:
+            needToSend = True
             print('Dropped Client: ', c)
             player = {}
             player['id'] = str(c)
@@ -76,9 +82,11 @@ def cleanClients(sock):
             del clients[c]
             clients_lock.release()
 
-      dm = json.dumps(deleteMessage)
-      for f in clients:
-         sock.sendto(bytes(dm,'utf8'), (f[0],f[1]))
+      if needToSend :
+         print('Sending DELETE message')
+         dm = json.dumps(deleteMessage)
+         for f in clients:
+            sock.sendto(bytes(dm,'utf8'), (f[0],f[1]))
 
       time.sleep(1)
 
@@ -95,7 +103,7 @@ def gameLoop(sock):
          clients[c]['color'] = {"R": random.random(), "G": random.random(), "B": random.random()}         
          player['id'] = str(c)
          player['color'] = clients[c]['color']
-         player['position'] = clienrs[c]['position']
+         player['position'] = clients[c]['position']
          GameState['players'].append(player)
       s=json.dumps(GameState)
       print(s)
